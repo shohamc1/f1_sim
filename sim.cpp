96c336cdb1d9ff5a04d007b99e3963095a243a32
shohamessi <shhmchk@gmail.com>
2018-01-21 18:19:57 +0530

    squashed bugs

--- sim.cpp
+++ sim.cpp
@@ -7,20 +7,27 @@ using namespace std;
 int main() {
     srand (time (0));
 
-    double times [3] = {100, 100, 100};
-    double skill [3] = {95, 90, 85};
+    double times [3] = {83.251, 83.456 , 85.425};
+    double skill [3] = {90, 95, 100};
     double r[3];
     double final [3];
+
+//    for (int i = 0; i < 3; i++) {
+//        cout << "Enter time for driver " << i+1 << ": ";
+//        cin >> times [i];
+//        cout << "Enter skill level of driver " << i + 1 << ": ";
+//        cin >> skill [i];
+//    }
 
     for (int i = 0; i < 3; i++) {
         r[i] = ((double) rand() / (RAND_MAX));
     }
 
     for (int i = 0; i < 3; i++) {
-       final [i] = times[i] + pow (100 - skill[i], r [i]);
+       final [i] = times[i] + pow (102 - skill[i], r [i]);
     }
 
     for (int i = 0; i < 3; i++) {
-        cout << i+1 << "\t" << final [i] << endl;
+        cout << i+1 << "\t" << final [i]  << "\t" << r[i] << endl;
     }
 }
